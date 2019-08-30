1e911ff71fdc095e56de8627001e6a2a87d8d114
TwilightxSunrise <54708923+TwilightxSunrise@users.noreply.github.com>
2019-08-30 20:33:21 +0800

    Delete numberWithCommas.js

--- functions/numberWithCommas.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (x) {
-    console.log('+numberWithCommas.js')
-    console.log('-numberWithCommas.js');
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
